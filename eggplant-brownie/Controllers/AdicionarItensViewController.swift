bd1fa2ad7a49d56f90e830e868120c43a1d666dd
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2021-06-06 15:17:00 -0300

    Adicionado arquivo xib para adicionar itens

---
+++ eggplant-brownie/Controllers/AdicionarItensViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AdicionarItensViewController.swift
+//  eggplant-brownie
+//
+//  Created by Eduardo Santos on 06/06/21.
+//
+
+import UIKit
+
+class AdicionarItensViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
