1446c70650b228d3128a5fed2d726a939c38b0a4
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2021-06-05 23:42:24 -0300

    Adicionado método de popview para remover uma view que está por cima de outra

--- eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/Controllers/ViewController.swift
@@ -33,6 +33,7 @@ class ViewController: UIViewController {
         
         let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade)
         print("comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
+        navigationController?.popViewController(animated: true)
     }
 }
 
