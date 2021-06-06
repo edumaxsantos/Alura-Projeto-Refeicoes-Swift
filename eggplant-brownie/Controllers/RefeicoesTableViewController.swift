8a4f483cc24ddbb33d4ba77f67fd7055d8306386
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2021-06-06 19:21:37 -0300

    Adicionado forma de capturar a célula que está sendo pressionada e capturar a refeição correta

--- eggplant-brownie/Controllers/RefeicoesTableViewController.swift
+++ eggplant-brownie/Controllers/RefeicoesTableViewController.swift
@@ -35,7 +35,13 @@ class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDeleg
     
     @objc func mostrarDetalhes(_ gesture: UILongPressGestureRecognizer) {
         if gesture.state == .began {
-            print("LongPress gesture")
+            let celula = gesture.view as! UITableViewCell
+            guard let indexPath = tableView.indexPath(for: celula) else { return }
+            
+            let refeicao = refeicoes[indexPath.row]
+            
+            print(refeicao.nome)
+            
         }
         
     }
